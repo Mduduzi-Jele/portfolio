105e0f425967320a2488abf1f0e333fd4e81b1f7
Mduduzi-Jele <jelemduduzisa@gmail.com>
2024-01-11 11:24:02 +0200

    Updated portfolio

--- index.js
+++ index.js
@@ -1,42 +1,54 @@
 const elements = document.querySelectorAll('.project-item');
 
 let projects = [{
-  h: "Content management site for south African public schools",
-  p: "A blog site serves as an online platform where individuals, businesses, or organizations share a diverse range of content, providing insights, information, and perspectives on various topics. These digital spaces have become instrumental in fostering communication and community engagement. A well-designed blog site often features a user-friendly interface, allowing readers to easily navigate through a collection of articles, opinion pieces, and multimedia content. It serves as a dynamic space for writers to express their thoughts, expertise, and creativity while fostering interaction through comments and social media integration. Whether focusing on lifestyle, technology, business, or personal narratives, a blog site acts as a virtual forum.",
-  imgUrl: `./assets/portfolio-images/blogpost.jpg`
+  h: "blog site site for south African public schools",
+  p: "This school blog site is the first phase of a large system aimed at improving the quality of education in south african public schools. For this part, the school blog acts as a dynamic showcase for the school's achievements, talents, and academic prowess, contributing to its positive online presence and attracting prospective students and educators. The end goal for this system is to provide student with everything they need for a better education and a brighter future",
+  imgUrl: `./assets/portfolio-images/thuto.png`,
+  projectUrl: "https://jazzy-pie-6d71dc.netlify.app/",
+  timeframe: "2023-Present",
 },
 {
   h: "My personal blog ",
-  p: "A blog site serves as an online platform where individuals, businesses, or organizations share a diverse range of content, providing insights, information, and perspectives on various topics. These digital spaces have become instrumental in fostering communication and community engagement. A well-designed blog site often features a user-friendly interface, allowing readers to easily navigate through a collection of articles, opinion pieces, and multimedia content. It serves as a dynamic space for writers to express their thoughts, expertise, and creativity while fostering interaction through comments and social media integration. Whether focusing on lifestyle, technology, business, or personal narratives, a blog site acts as a virtual forum.",
-  imgUrl: "./assets/portfolio-images/Customer-Service-Software.jpg"
+  p: "In this platform, I aim to write about all things tech and the businesses behind some of the greatest innovations in the technology sector.I plan to delve into the world of technology and the businesses driving significant innovations in the sector. From the latest advancements in artificial intelligence to the entrepreneurial landscape shaping startups, my blog aims to provide an in-depth exploration of the ever-changing tech scene. Based in Johannesburg, South Africa, and with a background in software development, my perspective will offer a global outlook, considering regional impacts and contributing to a well-rounded understanding of the tech ecosystem",
+  imgUrl: "./assets/portfolio-images/matterhorn.png",
+  projectUrl: "https://blog-production-8bc9.up.railway.app/",
+  timeframe: "2023-Present"
 },
 {
   h: "A landing page for Dronoticz",
-  p: "A blog site serves as an online platform where individuals, businesses, or organizations share a diverse range of content, providing insights, information, and perspectives on various topics. These digital spaces have become instrumental in fostering communication and community engagement. A well-designed blog site often features a user-friendly interface, allowing readers to easily navigate through a collection of articles, opinion pieces, and multimedia content. It serves as a dynamic space for writers to express their thoughts, expertise, and creativity while fostering interaction through comments and social media integration. Whether focusing on lifestyle, technology, business, or personal narratives, a blog site acts as a virtual forum.",
-  imgUrl: "./assets/portfolio-images/diagnostics.jpg"
+  p: "This is a landing page for a robotics start based in joburg.The client wanted a simple, minimal design that would tell people what they are about.",
+  imgUrl: "./assets/portfolio-images/dronoticz.png",
+  projectUrl: "https://mduduzi-jele.github.io/website-two/",
+  timeframe: "2023-Present"
 },
 {
   h: "A website for people to create and share surveys.",
-  p: "A blog site serves as an online platform where individuals, businesses, or organizations share a diverse range of content, providing insights, information, and perspectives on various topics. These digital spaces have become instrumental in fostering communication and community engagement. A well-designed blog site often features a user-friendly interface, allowing readers to easily navigate through a collection of articles, opinion pieces, and multimedia content. It serves as a dynamic space for writers to express their thoughts, expertise, and creativity while fostering interaction through comments and social media integration. Whether focusing on lifestyle, technology, business, or personal narratives, a blog site acts as a virtual forum.",
-  imgUrl: "./assets/portfolio-images/market-research-intellect.png"
+  p: "",
+  imgUrl: "./assets/portfolio-images/savvy.png",
+  projectUrl: "https://savvvy.netlify.app/",
+  timeframe: "2023-Present"
 },
 {
   h: "A simple react file upload site.",
-  p: "A blog site serves as an online platform where individuals, businesses, or organizations share a diverse range of content, providing insights, information, and perspectives on various topics. These digital spaces have become instrumental in fostering communication and community engagement. A well-designed blog site often features a user-friendly interface, allowing readers to easily navigate through a collection of articles, opinion pieces, and multimedia content. It serves as a dynamic space for writers to express their thoughts, expertise, and creativity while fostering interaction through comments and social media integration. Whether focusing on lifestyle, technology, business, or personal narratives, a blog site acts as a virtual forum.",
-  imgUrl: "./assets/portfolio-images/technical-illustration-software.png"
+  p: "This is a simple website I built to improve my skills of designing user interfaces and also implement some react js concepts that I learned during this time.",
+  imgUrl: "./assets/portfolio-images/stream.png",
+  projectUrl: "https://streamm.netlify.app/",
+  timeframe: "2023-Present"
 },
 {
   h: "A react clock",
-  p: "A blog site serves as an online platform where individuals, businesses, or organizations share a diverse range of content, providing insights, information, and perspectives on various topics. These digital spaces have become instrumental in fostering communication and community engagement. A well-designed blog site often features a user-friendly interface, allowing readers to easily navigate through a collection of articles, opinion pieces, and multimedia content. It serves as a dynamic space for writers to express their thoughts, expertise, and creativity while fostering interaction through comments and social media integration. Whether focusing on lifestyle, technology, business, or personal narratives, a blog site acts as a virtual forum.",
-  imgUrl: "./assets/portfolio-images/UML-diagrams.png"
+  p: "Named after one of my favorite watches the rolex white face aka panda, this is the first project I have ever built by myself. The purpose of this project was to implement programming logic skills that I learned in school. This is a time",
+  imgUrl: "./assets/portfolio-images/panda.png",
+  projectUrl: "https://cheerful-marigold-88c03d.netlify.app/",
+  timeframe: "2020-Present"
 },
 ]
 
 for (let i = 0; i < projects.length; i++) {
   elements[i].addEventListener("mouseover", () => {
     document.getElementById("h").textContent = projects[i].h
-    // elements[i].style.width = 
     document.getElementById("p").textContent = projects[i].p
+    document.getElementById("time").textContent = projects[i].timeframe
     document.getElementById("imgUrl").style.display = "inline"
     document.getElementById("imgUrl").src = projects[i].imgUrl
   })
@@ -44,7 +56,8 @@ for (let i = 0; i < projects.length; i++) {
 
 for (let i = 0; i < elements.length; i++) {
   elements[i].addEventListener("mouseout", () => {
-    document.getElementById("h").textContent = "3 years of React and Java"
+    document.getElementById("h").textContent = "3 years of javascript and Java"
+    document.getElementById("time").textContent = "2020-Present"
     document.getElementById("p").textContent = `Default. A blog site serves as an online platform where individuals,
     businesses, or organizations share a diverse range of content, providing insights, information, and perspectives
     on various topics. These digital spaces have become instrumental in fostering communication and community
@@ -66,5 +79,4 @@ checkbox.addEventListener("click", function () {
   } else {
     home__nav__menu.style.display = "none";
   }
-  console.log("Hello");
 });
